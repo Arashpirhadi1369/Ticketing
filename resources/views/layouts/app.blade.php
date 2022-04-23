349debc49b7d18aa188627ee379f090ff1beeb0d
Massoud Ghassemi Taj <massoud.taj@gmail.com>
2022-04-23 22:27:42 +0430

    add Bootstrap and Fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,13 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link href="{{asset('css/fontiran.css')}}" rel="stylesheet" type="text/css"/>
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{asset('/css/bootstrap-rtl.css') }}" >
+        <link rel="stylesheet" href="{{asset('/css/fontiranstyle.css') }}" >
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -33,4 +34,19 @@
             </main>
         </div>
     </body>
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/jquery.slim.min.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+            crossorigin="anonymous"></script>
+
 </html>
