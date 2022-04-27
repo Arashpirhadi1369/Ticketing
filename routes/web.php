5f9759fcaf81e7a3e4ff846825ad526475046a4b
unknown <Arash.cisco.1990@gmail.com>
2022-04-27 11:26:37 +0430

    added firewall

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,12 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::group(['middleware' => 'firewall.all'], function () {
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    Route::get('/', function () {
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
+});
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
