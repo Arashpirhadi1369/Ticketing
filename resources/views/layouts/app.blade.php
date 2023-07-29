ea228f37266a93b3f7139a3c15eb06af07a528a5
unknown <Administrator@MBACO.LOCAL>
2023-07-29 11:34:15 +0330

    fixed title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'سامانه درخواست های واحد فناوری اطلاعات') }}</title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link href="{{asset('css/fontiran.css')}}" rel="stylesheet" type="text/css" />
